15729e4fca160ac56177f075bf4f20618a6b8021
AlexRamey <alex.b.ramey@gmail.com>
2014-10-28 21:25:31 -0400

    1. Merchandise Done 2. Settings Bundle Done 3. Twitter Pointed to Live #s

--- VAMUN_Prototype-iOS/VAMMerchandiseViewController.m
+++ VAMUN_Prototype-iOS/VAMMerchandiseViewController.m
@@ -22,8 +22,9 @@
     {
         //custom initialization
         yOffset = 0;
-        merchandiseInfo = @[@"Sunglasses - $5", @"Cups - $5"];
-        merchandiseImageNames = @[@"vamun_merch_sunglasses", @"vamun_merch_cup"];
+        
+        merchandiseInfo = @[@"Short Sleeve T-Shirts - $9.99", @"Long Sleeve T-Shirts - $14.99", @"Sunglasses - $3.99", @"Metal Water Bottles - $9.99", @"Cups - $3.99"];
+        merchandiseImageNames = @[@"vamun_merch_shortsleevetshirt", @"vamun_merch_longsleevetshirt", @"vamun_merch_sunglasses", @"vamun_merch_metalwaterbottle", @"vamun_merch_cup"];
     }
     
     return self;
@@ -33,18 +34,56 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
     for (int i = 0; i < [merchandiseInfo count]; i++)
     {
         [_scrollView addSubview:[[VAMMerchandiseCard alloc] initWithImage:[UIImage imageNamed:merchandiseImageNames[i]] info:merchandiseInfo[i] offset:yOffset callback:self]];
     }
-    for (int i = 0; i < [merchandiseInfo count]; i++)
+    
+    //Add deals card at the bottom
+    
+    VAMMerchandiseCard *dealCard = [[VAMMerchandiseCard alloc] initWithImage:nil info:nil offset:yOffset callback:self];
+    
+    for (UIView *view in dealCard.subviews)
     {
-        [_scrollView addSubview:[[VAMMerchandiseCard alloc] initWithImage:[UIImage imageNamed:merchandiseImageNames[i]] info:merchandiseInfo[i] offset:yOffset callback:self]];
+        [view removeFromSuperview];
     }
-    for (int i = 0; i < [merchandiseInfo count]; i++)
-    {
-        [_scrollView addSubview:[[VAMMerchandiseCard alloc] initWithImage:[UIImage imageNamed:merchandiseImageNames[i]] info:merchandiseInfo[i] offset:yOffset callback:self]];
-    }
+    
+    UILabel *dealTitle = [[UILabel alloc] init];
+    dealTitle.numberOfLines = 0;
+    dealTitle.text = @"Combos";
+    dealTitle.textAlignment = NSTextAlignmentCenter;
+    dealTitle.font = [UIFont fontWithName:@"HelveticaNeue-Bold" size:16.0];
+    [dealTitle sizeToFit];
+    dealTitle.frame = CGRectMake(10.0, 5, dealCard.frame.size.width - 20.0, dealTitle.frame.size.height);
+    
+    UILabel *deal1 = [[UILabel alloc] init];
+    deal1.numberOfLines = 0;
+    deal1.text = @"Long Sleeve T-Shirt + Sunglasses - $16.99";
+    deal1.font = [UIFont fontWithName:@"HelveticaNeue-Light" size:16.0];
+    [deal1 sizeToFit];
+    deal1.frame = CGRectMake(10.0, 5 + dealTitle.frame.size.height + 5, dealCard.frame.size.width - 20.0, deal1.frame.size.height);
+    
+    UILabel *deal2 = [[UILabel alloc] init];
+    deal2.numberOfLines = 0;
+    deal2.text = @"Short Sleeve T-Shirt + Sunglasses - $11.99";
+    deal2.font = [UIFont fontWithName:@"HelveticaNeue-Light" size:16.0];
+    [deal2 sizeToFit];
+    deal2.frame = CGRectMake(10.0, 5 + dealTitle.frame.size.height + 5 + deal1.frame.size.height + 5, dealCard.frame.size.width - 20.0, deal2.frame.size.height);
+    
+    UILabel *deal3 = [[UILabel alloc] init];
+    deal3.numberOfLines = 0;
+    deal3.text = @"Free Sunglasses Come with Purchase of a T-Shirt and Metal Water Bottle";
+    deal3.font = [UIFont fontWithName:@"HelveticaNeue-Light" size:16.0];
+    [deal3 sizeToFit];
+    deal3.frame = CGRectMake(10.0, 5 + dealTitle.frame.size.height + 5 + deal1.frame.size.height + 5 + deal2.frame.size.height + 5, dealCard.frame.size.width - 20.0, deal3.frame.size.height * 2);
+    
+    [dealCard addSubview:dealTitle];
+    [dealCard addSubview:deal1];
+    [dealCard addSubview:deal2];
+    [dealCard addSubview:deal3];
+    
+    [_scrollView addSubview:dealCard];
     
 }
 
